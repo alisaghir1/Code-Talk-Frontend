20e91de4ed142ba2a6155a37ebccb147d4e1f0bc
alisaghir1 <alisaghir543@gmail.com>
2024-02-19 20:05:21 +0200

    finished the sign in page plus adding validators and tailwind config for global css and finished the backend with appwrite

--- src/authentication/forms/SignupForm.tsx
+++ src/authentication/forms/SignupForm.tsx
@@ -1,8 +1,109 @@
-import React from 'react'
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form"
+import { signupValidation } from "@/lib/validation"
+import Loader from "@/components/ui/global components/Loader"
+import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
+ 
 
 const SignupForm = () => {
+  const isLoading = true
+
+   // 1. Define your form.
+   const form = useForm<z.infer<typeof signupValidation>>({
+    resolver: zodResolver(signupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: ""
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof signupValidation>) {
+    const newUser = await createUserAccount(values)
+
+    console.log(newUser)
+  }
+
   return (
-    <div>SignupForm</div>
+    <Form {...form}>
+      <div className="smLw-420 flex-center flex-col">
+        <img src="logo" alt="logo" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+        <p className="text-light-3 small-medium md:base-regular">To use CodeTalk please register down with your details</p>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input type="text" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+            <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input type="text" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+            <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input type="email" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+            <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <Input type="password" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button className="shad-button_primary" type="submit">
+        {isLoading ?
+        <div className="flex-center gap-2">
+          <Loader />
+        </div> : 'Sign Up'
+        }
+        </Button>
+        <p className="text-small-regular text-light-2 text-center mt-2">
+          Already have an account?
+          <Link className="text-primary-500 text-small-semibold ml-1" to={'/sign-in'}>Log in</Link>
+        </p>
+    </form>
+    </div>
+  </Form>
   )
 }
 
---
+++ src/components/ui/global components/Loader.tsx
@@ -0,0 +1,19 @@
+import './Loader.css'
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <div className="loader">
+        <span className="l">L</span>
+        <span className="o">o</span>
+        <span className="a">a</span>
+        <span className="d">d</span>
+        <span className="i">i</span>
+        <span className="n">n</span>
+        <span className="g">g</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
